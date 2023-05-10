77ba976da50022c82d97d886cf5a659efa1e7d9d
김시후 <me@iamsihu.wtf>
2023-05-11 01:24:42 +0900

    implement 'help'

--- include/basic_commands.h
+++ include/basic_commands.h
@@ -15,7 +15,23 @@ using namespace std;
 namespace fs = std::filesystem;
 
 void helpCmd(const vector<string> &args) {
-  info("system.wip");
+  if (args.size() == 1) {
+    vector<string> helps;
+    for (const auto &item : commands) {
+      Command &command = *item;
+      string text = command.getName() + ": " + command.getDescription();
+      helps.push_back(text);
+    }
+
+    info("Only some commands are shown:");
+    info("\033[90m(Type shfl cmds to see all commands.)");
+    for (const auto &item : helps) info(item);
+  } else if (args.size() == 2) {
+    info("system.wip"); // TODO
+  } else {
+    too_many_arguments();
+    return;
+  }
 }
 
 void shflCmd(const vector<string> &args) {
