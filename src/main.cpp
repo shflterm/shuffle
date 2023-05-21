fedd5606ec37b336228e0da2e9da6c480f79a16e
김시후 <me@iamsihu.wtf>
2023-05-21 15:34:22 +0900

    single line execution

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,28 @@
 
 using namespace std;
 
-int main() {
-  cout << "Loading Shuffle...\n";
-  loadLanguageFile("en_us");
+int main(int argc, char *argv[]) {
   loadCommands();
   ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
-  clear();
+  if (argc > 1) {
+    string arg = argv[1];
+    if (arg == "--version" || arg == "-v") {
+      cout << "Shuffle " << SHUFFLE_VERSION << endl;
+      return 0;
+    }
 
+    Workspace workspace;
+    string cmd;
+    for (int i = 1; i < argc; ++i) {
+      cmd += argv[i];
+      cmd += " ";
+    }
+    workspace.execute(cmd);
+    return 0;
+  }
+
+  clear();
   info("Welcome to " + FG_YELLOW + "SHUFFLE " + SHUFFLE_VERSION + RESET);
   info("(C) 2023 Shuffle Team. All Rights Reserved.");
   white();
