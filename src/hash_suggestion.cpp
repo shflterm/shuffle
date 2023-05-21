0179fb1bc0958819f8535bac4b5d9428c466ef32
김시후 <me@iamsihu.wtf>
2023-05-21 15:47:05 +0900

    smart hints

--- src/hash_suggestion.cpp
+++ src/hash_suggestion.cpp
@@ -1,8 +1,10 @@
+#include "hash_suggestion.h"
+
 #include <vector>
 
-#include "hash_suggestion.h"
-#include "utils/utils.h"
 #include "console.h"
+#include "commandmgr.h"
+
 OpenAI oai;
 Conversation convo;
 
@@ -11,24 +13,13 @@ bool initialized = false;
 void initialize() {
   convo = Conversation();
 
-  vector<string> commands;
+  vector<string> hints;
   // TODO: This is for tests.
-  commands.emplace_back("help: Print a list of commands");
-  commands.emplace_back("clear: Clear console");
-  commands.emplace_back("cd [dir]: Change directory");
-  commands.emplace_back("list: Print list file of current directory");
-  commands.emplace_back("mk [name]: Create a file or directory named [name]");
-  commands.emplace_back("rm [name]: Delete a file or directory named [name]");
-  commands.emplace_back("cp [from] [to]: Copy file [from] to [to].");
-  commands.emplace_back("mv [from] [to]: Move file [from] to [to].");
-  commands.emplace_back("dir: Print current directory");
-  commands.emplace_back("find [target]: Search for files with the name [target] or containing [target]");
-  commands.emplace_back("zip [file]: Compress a file (directory) [file] into zip format");
-  commands.emplace_back("zip [zipFile]: Uncompress [zip_file], which is a file in .zip format");
-  commands.emplace_back("open: Open the current directory in explorer");
-  commands.emplace_back("open [file]: open [file] in explorer");
-  commands.emplace_back("get [url]: Download file from [url].");
-  commands.emplace_back("get [url] [newFile]: Download file from [url] and save to [newFile].");
+  for (const auto &item : commands) {
+    Command &command = *item;
+    string text = command.getName() + ": " + command.getDescription();
+    hints.push_back(text);
+  }
 
   string systemData = "You are a bot that recommends which commands to use in certain situations."
                       "The command doesn't have to be a single line."
@@ -38,7 +29,7 @@ void initialize() {
                       "Also, when outputting a command, it must start with '```` and end with '````."
                       "If a user asks a question about a language other than Shuffle (such as C++ or Java) or another topic, the answer is: \"Sorry, but I can only recommend the Shuffle command. I don't know much about the rest.\". Also, never give any other answer."
                       "Below is a list of commands you can use and what they do.";
-  for (const auto &item : commands) systemData += item + "\n";
+  for (const auto &item : hints) systemData += item + "\n";
 
   convo.SetSystemData(systemData);
 
@@ -49,7 +40,7 @@ string callGptAI(const string &prompt) {
   if (!initialized) initialize();
 
   convo.AddUserData(prompt);
-  if (oai.auth.SetKey("[secret]")) {
+  if (oai.auth.SetKey("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")) {
     try {
       auto fut = oai.ChatCompletion->create_async(
           "gpt-3.5-turbo", convo
