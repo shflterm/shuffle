c45eb6cff802b49530afb8fb5c356f0ba9dc82c0
김시후 <me@iamsihu.wtf>
2023-05-27 22:56:59 +0900

    Patch: MacOS error

---
+++ include/version.h
@@ -0,0 +1,27 @@
+#ifndef SHUFFLE_INCLUDE_VERSION_H_
+#define SHUFFLE_INCLUDE_VERSION_H_
+
+#include <string>
+#include <sstream>
+
+#define SHUFFLE_VERSION Version(1, 0, 0, 0)
+
+class Version {
+  int major, minor, patch, hotfix;
+
+ public:
+  Version(int major, int minor, int patch, int hotfix) : major(major), minor(minor), patch(patch), hotfix(hotfix) {}
+
+  [[nodiscard]] string to_string() const {
+    stringstream ss;
+    ss << "v";
+    ss << major << ".";
+    ss << minor << ".";
+    ss << patch << ".";
+    ss << hotfix;
+
+    return string(ss.str());
+  }
+};
+
+#endif //SHUFFLE_INCLUDE_VERSION_H_
