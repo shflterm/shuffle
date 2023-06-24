ef596692bcb3292acda97aea021e2dfb108d03ee
syuk <08sihu@gmail.com>
2023-06-24 16:00:31 +0900

    Usage

--- src/commandmgr.cpp
+++ src/commandmgr.cpp
@@ -15,125 +15,131 @@ using namespace std;
 vector<shared_ptr<Command>> commands;
 
 void loadDefaultCommands() {
-  commands.push_back(make_shared<Command>(Command(
-      "shfl", "Shuffle Command",
-      {
-          Command("reload", "Reload all commands"),
-          Command("apps", "Manage SAPP",
-                  {Command("add", "Install SAPP"),
-                   Command("remove", "Delete SAPP")}),
-          Command("update", "Update Shuffle"),
-          Command("credits", "Show credits"),
-      })));
-  commands.push_back(make_shared<Command>(Command(
-      "help", "Show help"
-      )));
+    commands.push_back(make_shared<Command>(Command(
+            "shfl", "Shuffle Command",
+            {
+                    Command("reload", "Reload all commands"),
+                    Command("apps", "Manage SAPP",
+                            {Command("add", "Install SAPP"),
+                             Command("remove", "Delete SAPP")}),
+                    Command("update", "Update Shuffle"),
+                    Command("credits", "Show credits"),
+            })));
+    commands.push_back(make_shared<Command>(Command(
+            "help", "Show help"
+    )));
 }
 
 void loadCommand(const CommandData &data) {
-  for (const auto &item : commands) {
-    if (item->getName() == data.name) return;
-  }
-  commands.push_back(make_shared<SAPPCommand>(SAPPCommand(data.name)));
+    for (const auto &item: commands) {
+        if (item->getName() == data.name) return;
+    }
+    commands.push_back(make_shared<SAPPCommand>(SAPPCommand(data.name)));
 }
 
 vector<CommandData> getRegisteredCommands() {
-  vector<CommandData> res;
+    vector<CommandData> res;
 
-  Json::Value commandList = getShflJson("apps");
-  for (auto command : commandList) {
-    CommandData data;
-    data.name = command["name"].asString();
-    res.push_back(data);
-  }
+    Json::Value commandList = getShflJson("apps");
+    for (auto command: commandList) {
+        CommandData data;
+        data.name = command["name"].asString();
+        res.push_back(data);
+    }
 
-  return res;
+    return res;
 }
 
 bool addRegisteredCommand(const CommandData &data) {
-  vector<CommandData> res;
+    vector<CommandData> res;
 
-  Json::Value commandList = getShflJson("apps");
+    Json::Value commandList = getShflJson("apps");
 
-  Json::Value value(Json::objectValue);
-  value["name"] = data.name;
-  for (const auto &item : commands) {
-    if (item->getName() == data.name) return false;
-  }
+    Json::Value value(Json::objectValue);
+    value["name"] = data.name;
+    for (const auto &item: commands) {
+        if (item->getName() == data.name) return false;
+    }
 
-  commandList.append(value);
+    commandList.append(value);
 
-  setShflJson("apps", commandList);
-  return true;
+    setShflJson("apps", commandList);
+    return true;
 }
 
 void loadCommands() {
-  commands.clear();
+    commands.clear();
 
-  loadDefaultCommands();
+    loadDefaultCommands();
 
-  for (const CommandData &command : getRegisteredCommands()) {
-    loadCommand(command);
-  }
+    for (const CommandData &command: getRegisteredCommands()) {
+        loadCommand(command);
+    }
 }
 
 shared_ptr<Command> findCommand(const string &name, const vector<shared_ptr<Command>> &DICTIONARY) {
-  for (auto &item : DICTIONARY) {
-    if (item->getName() == name) {
-      return item;
+    for (auto &item: DICTIONARY) {
+        if (item->getName() == name) {
+            return item;
+        }
     }
-  }
-  return nullptr;
+    return nullptr;
 }
 
 shared_ptr<Command> findCommand(const string &name) {
-  return findCommand(name, commands);
+    return findCommand(name, commands);
 }
 
 const string &Command::getName() const {
-  return name;
+    return name;
 }
 
 const string &Command::getDescription() const {
-  return description;
-}
-
-const string &Command::getValue() const {
-  return value;
+    return description;
 }
 
 vector<shared_ptr<Command>> Command::getChildren() const {
-  vector<shared_ptr<Command>> newChildren;
-  for (auto child : children) {
-    newChildren.push_back(make_shared<Command>(child));
-  }
-  return newChildren;
+    vector<shared_ptr<Command>> newChildren;
+    for (auto child: children) {
+        newChildren.push_back(make_shared<Command>(child));
+    }
+    return newChildren;
 }
 
 void Command::addChild(const Command &command) {
-  children.push_back(command);
+    children.push_back(command);
 }
 
 void Command::run(Workspace &ws, const vector<std::string> &args) const {}
 
+Command::Command(string name, string description, vector<Command> children, vector<pair<string, string>> usage)
+        : name(std::move(name)),
+          description(std::move(description)),
+          children(std::move(children)),
+          usage(std::move(usage)) {}
+
 Command::Command(string name, string description, vector<Command> children)
-    : name(std::move(name)),
-      description(std::move(description)),
-      children(std::move(children)) {}
+        : name(std::move(name)),
+          description(std::move(description)),
+          children(std::move(children)) {}
 
 Command::Command(string name, string description)
-    : name(std::move(name)),
-      description(std::move(description)) {}
+        : name(std::move(name)),
+          description(std::move(description)) {}
 
 Command::Command(string name, vector<Command> children)
-    : name(std::move(name)),
-      description("-"),
-      children(std::move(children)) {}
+        : name(std::move(name)),
+          description("-"),
+          children(std::move(children)) {}
 
 Command::Command(string name)
-    : name(std::move(name)),
-      description("-") {}
+        : name(std::move(name)),
+          description("-") {}
+
+const vector<pair<string, string>> &Command::getUsage() const {
+    return usage;
+}
 
 OptionSubCommand::OptionSubCommand(const string &name, string description)
-    : Command("option." + name,
-              std::move(description)) {}
+        : Command("option." + name,
+                  std::move(description)) {}
