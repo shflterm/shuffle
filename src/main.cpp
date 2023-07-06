a9745f19bd400f8927391a53b0f13256f16c1dd1
syuk <08sihu@gmail.com>
2023-07-07 01:20:11 +0900

    options 파싱 기능 & 명령어 시스템 변경

--- src/main.cpp
+++ src/main.cpp
@@ -90,6 +90,6 @@ int main(int argc, char *argv[]) {
 
     currentWorkspace = new Workspace("main");
     while (true) {
-        currentWorkspace->inputPrompt(true);
+        currentWorkspace->inputPrompt(false);
     }
 }
