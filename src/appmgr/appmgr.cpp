d61ee42b5432e7c75e273f0225a9598749a017f5
ssyuk <sihu@syuk.me>
2023-12-29 11:20:25 +0900

    🧹 Removed unused code

--- src/appmgr/appmgr.cpp
+++ src/appmgr/appmgr.cpp
@@ -44,46 +44,6 @@ namespace appmgr {
         return res;
     }
 
-    // vector<string> App::makeDynamicSuggestion(Workspace&ws, const string&suggestId) const {
-    //     // Create workspace table
-    //     lua_newtable(L); {
-    //         lua_pushstring(L, ws.currentDirectory().string().c_str());
-    //         lua_setfield(L, -2, "dir");
-    //     } // ws.dir
-    //
-    //     lua_setglobal(L, "workspace");
-    //
-    //     // Create args list
-    //     lua_pushstring(L, suggestId.c_str());
-    //     lua_setglobal(L, "suggestId");
-    //
-    //     lua_getglobal(L, "suggest");
-    //     if (lua_type(L, -1) != LUA_TFUNCTION) {
-    //         error("Error: 'suggest' is not a function!");
-    //         return {};
-    //     }
-    //     if (lua_pcall(L, 0, 0, 0)) {
-    //         error("\nAn error occurred while generating suggestion.\n\n" + string(lua_tostring(L, -1)));
-    //     }
-    //
-    //     const lua_Unsigned tableSize = lua_rawlen(L, -1);
-    //
-    //     if (lua_istable(L, -1)) {
-    //         vector<string> resultArray;
-    //         for (int i = 1; i <= tableSize; ++i) {
-    //             lua_rawgeti(L, -1, i);
-    //
-    //             if (lua_isstring(L, -1)) {
-    //                 resultArray.emplace_back(lua_tostring(L, -1));
-    //             }
-    //
-    //             lua_pop(L, 1);
-    //         }
-    //         return resultArray;
-    //     }
-    //     return {};
-    // }
-
     extern "C" {
     typedef string (*entrypoint_t)(Workspace*, const map<string, string>&, bool);
 
@@ -251,7 +211,7 @@ namespace appmgr {
 
         Json::Value apps = getShflJson("apps");
         bool alreadyExists = false;
-        for (const auto& app: apps) {
+        for (const auto&app: apps) {
             if (app.asString() == name) {
                 alreadyExists = true;
                 break;
